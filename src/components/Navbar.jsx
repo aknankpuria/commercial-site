d96b6a352534e447fe6ee5b34bdd9c435b7e68e3
Aslam Khan shah <aksi34012@gmail.com>
2023-08-11 23:17:26 +0530

    flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,9 +10,9 @@ const Navbar = () => {
           <div className="basic-1/4"> whtsapp</div>
           <div className="basic-1/4">email</div>
           <div className="basic-1/4 flex flex-row justify-between items-center">
-            <div className="basic-1/3 ">fb</div>
-            <div className="basic-1/3">insta</div>
-            <div className="basic-1/3">in </div>
+            <div className="basic-1/3 m-5">fb</div>
+            <div className="basic-1/3 m-5">insta</div>
+            <div className="basic-1/3 m-5">in </div>
           </div>
 
    </div>
